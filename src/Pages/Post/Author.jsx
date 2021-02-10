7f263dab4d5a2b6de50f504cd6e84c5a6f18105c
unknown <giovannemarrone@gmail.com>
2021-02-10 00:47:19 -0300

    practiced commit

--- src/Pages/Post/Author.jsx
+++ src/Pages/Post/Author.jsx
@@ -5,8 +5,10 @@ export const Author = () => {
     return (
 <div className='AuthorInfo'>
                 <p className='PublishedBy'>Publicado por</p> 
+                <div className='IconAndName'>
             <img className='AuthorImg' src='https://secure.gravatar.com/avatar/3822355100b05a36e2347e3c30cce9c4?s=180&r=g' alt='AuthorImg' />
                     <p className='AuthorID'>Giovanne Marrone</p>
+                    </div>
             </div>
     )
 }
--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import './Post.css'
-import { Author } from './Author'
-import { AuthorAbout } from './AuthorAbout'
-import { Comments } from './Comments'
+import React from 'react';
+import './Post.css';
+import { Author } from './Author';
+import { AuthorAbout } from './AuthorAbout';
+import { Comments } from './Comments';
 
 export const Post = () => {
-    return (
-        <div className='PostPage'>
-            <div className='PostTitle'>
-                <h1>Disney+ anuncia a série oficial da Ahsoka como um spin-off de O Mandaloriano</h1>
-            </div>
-            <Author />
-            <div className='PostContent'>
-                <div className='SideInfo'>
-                    <p className='SideDefault'>Tags:</p>
-                    <p className='PostTags'>Disney+ / Ahsoka Tano / Série / Disney</p>
-                    <p className='SideDefault'>Publicado em:</p>
-                    <p className='PostDate'>05/02/2021 / 19:30</p>
-                </div>
-                <div className='PostText'>
-                    <p>Conteúdo do post*</p>
-                </div>
-            </div>
-            <AuthorAbout />
-            <Comments/>
-        </div>
-    )
-}
+	return (
+		<div className="PostPage">
+			<div className='PostTitleContainer'>
+				<div className="PostTitle">
+					<h1>
+						Disney+ anuncia a série oficial da Ahsoka como um
+						spin-off de O Mandaloriano
+					</h1>
+				</div>
+			</div>
+			{/* <Author /> */}
+			<div className="PostContent">
+				<div className="SideInfo">
+                <Author />
+					<p className="SideDefault">Tags:</p>
+					<p className="PostTags">
+						Disney+ / Ahsoka Tano / Série / Disney
+					</p>
+					<p className="SideDefault">Publicado em:</p>
+					<p className="PostDate">05/02/2021 / 19:30</p>
+				</div>
+				<div className="PostText">
+					<p>Conteúdo do post*</p>
+				</div>
+			</div>
+			<AuthorAbout />
+			<Comments />
+		</div>
+	);
+};
